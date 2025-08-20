0329a8fb4c3f8ed1643fff30f1ea21441ed23cd0
UAdharsh <adharshajay2001@gmail.com>
2025-08-20 04:21:28 -0400

    Sprint 2

--- service/__init__.py
+++ service/__init__.py
@@ -9,6 +9,9 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 from flask_talisman import Talisman
+from flask_cors import CORS
+from service import config
+from service.common import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
@@ -16,7 +19,7 @@ app.config.from_object(config)
 
 # Import the routes After the Flask app is created
 talisman = Talisman(app)
-
+CORS(app)
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
 
